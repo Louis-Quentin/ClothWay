d1c07d757cb8282e723829c37b2e09895f8c1ed2
Amsellem Louis-Quentin <louis-quentin.amsellem@epitech.eu>
2022-12-08 00:41:34 +0100

    [ADD] routes register & login are now parsed and error handling is added, when a request succes (request type : OK) is sent back

--- sources/Back/sources/router.go
+++ sources/Back/sources/router.go
@@ -1,39 +1,59 @@
 package router
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"io"
 )
+
+type User struct {
+	Login    string
+	Password string
+}
+
+type NewUser struct {
+	Login    string
+	Password string
+	Email    string
+}
 
 func Handle_home_request(context *gin.Context) {
 	var msg string
 	if err := context.ShouldBind(&msg); err != nil {
-		context.AbortWithError(http.StatusBadRequest, err)
+		context.JSON(400, gin.H{"Error": "Wrong home request format"})
 	} else {
-		fmt.Println(context.Request)
-		context.JSON(200, gin.H{"path": "home"})
+		context.JSON(200, gin.H{"Home": "OK"})
 	}
 }
 
 func Handle_register_request(context *gin.Context) {
-	var msg string
-
-	if err := context.ShouldBind(&msg); err != nil {
-		context.AbortWithError(http.StatusBadRequest, err)
+	var user NewUser
+	string_body, _ := io.ReadAll(context.Request.Body)
+	err := json.Unmarshal([]byte(string_body), &user)
+	if err != nil {
+		context.JSON(400, gin.H{"Error": "Wrong register request format"})
+	} else if user.Login == "" {
+		context.JSON(300, gin.H{"Error": "error no login given"})
+	} else if user.Password == "" {
+		context.JSON(300, gin.H{"Error": "error no password given"})
+	} else if user.Email == "" {
+		context.JSON(300, gin.H{"Error": "error no email given"})
 	} else {
-		fmt.Println(context.Request)
-		context.JSON(200, gin.H{"path": "register"})
+		context.JSON(200, gin.H{"Register": "OK"})
 	}
 }
 
 func Handle_login_request(context *gin.Context) {
-	var msg string
-
-	if err := context.ShouldBind(&msg); err != nil {
-		context.AbortWithError(http.StatusBadRequest, err)
+	var user User
+	string_body, _ := io.ReadAll(context.Request.Body)
+	err := json.Unmarshal([]byte(string_body), &user)
+	if err != nil {
+		context.JSON(400, gin.H{"Error": "Wrong login request format"})
+	} else if user.Login == "" {
+		context.JSON(300, gin.H{"Error": "error no login given"})
+	} else if user.Password == "" {
+		context.JSON(300, gin.H{"Error": "error no password given"})
 	} else {
-		fmt.Println(context.Request)
-		context.JSON(200, gin.H{"path": "login"})
+		context.JSON(200, gin.H{"Login": "OK"})
 	}
 }
