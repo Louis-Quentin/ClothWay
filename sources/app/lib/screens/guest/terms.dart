49ed9960926060e64294333b934323f64c8e8295
alexis.godiveau@alea-prevention.com <alexis.godiveau@alea-prevention.com>
2022-12-08 08:43:47 -0500

    [ADD] connexion page

---
+++ sources/app/lib/screens/guest/terms.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TermScreen extends StatefulWidget {
+  @override
+  _TermScreenState createState() => _TermScreenState();
+}
+
+class _TermScreenState extends State<TermScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
