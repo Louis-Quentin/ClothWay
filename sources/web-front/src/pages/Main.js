937e253cc6d4bfeb46810ef80db918faddaa539e
Amsellem Louis-Quentin <louis-quentin.amsellem@epitech.eu>
2022-12-13 13:20:21 +0100

    [ADD] shop_list style and build in js files

--- sources/web-front/src/pages/Main.js
+++ sources/web-front/src/pages/Main.js
@@ -11,80 +11,85 @@ import {
     alpha,
     InputBase,
     Stack
-  }
-  from '@mui/material';
+}
+    from '@mui/material';
+import SetShopList from "./Main_page_components/ShopList";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
+const Search = styled('div')(({theme}) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
+        marginLeft: theme.spacing(1),
+        width: 'auto',
     },
-  },
+}));
+
+const SearchIconWrapper = styled('div')(({theme}) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({theme}) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
 }));
 
 export default function Main() {
-  return (
-    <AppBar position='static' style={{ background: 'green' }}>
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Stack direction="row" alignItems="center">
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            arial-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ mr: 10 }}>
-            ClothWay
-          </Typography>
-        </Stack>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Recherche…"
-            inputProps={{ 'aria-label': 'search' }}
-          />
-        </Search>
-        <Button href="/test" sx={{ color: "inherit" }}> S'inscrire </Button>
-      </Toolbar>
-    </AppBar>
-  );
+    return (
+        <div>
+            <AppBar position='static' style={{background: 'green'}}>
+                <Toolbar sx={{justifyContent: "space-between"}}>
+                    <Stack direction="row" alignItems="center">
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            arial-label="menu"
+                            sx={{mr: 2}}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{mr: 10}}>
+                            ClothWay
+                        </Typography>
+                    </Stack>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon/>
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Recherche…"
+                            inputProps={{'aria-label': 'search'}}
+                        />
+                    </Search>
+                    <Button href="/test" sx={{color: "inherit"}}> S'inscrire </Button>
+                </Toolbar>
+            </AppBar>
+            <SetShopList>
+            </SetShopList>
+        </div>
+    );
 }
---
+++ sources/web-front/src/pages/Main_page_components/ItemToSell.js
@@ -0,0 +1,12 @@
+
+import React, {Component} from "react";
+
+export function ItemToSell(price, score, img) {
+    this.Img = img;
+    this.Price = price;
+    this.Score = score;
+}
+
+
+
+
