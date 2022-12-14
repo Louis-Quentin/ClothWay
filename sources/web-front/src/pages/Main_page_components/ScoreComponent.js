7389c88e6c356a2fff8a8204a31d247089ae253a
Amsellem Louis-Quentin <louis-quentin.amsellem@epitech.eu>
2022-12-14 14:23:04 +0100

    [ADD] Card ui remastered with score logo

---
+++ sources/web-front/src/pages/Main_page_components/ScoreComponent.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Typography from '@mui/material/Typography';
+import EnergySavingsLeafIcon from '@mui/icons-material/EnergySavingsLeaf';
+
+const StyledRating = styled(Rating)({
+    '& .MuiRating-iconFilled': {
+        color: '#2E8B57',
+    },
+    '& .MuiRating-iconHover': {
+        color: '#ff3d47',
+    },
+});
+
+export default function CustomizedRating() {
+    return (
+        <div className="style-item-score-box">
+            <Box
+                sx={{
+                    '& > legend': { mt: 2 },
+                }}
+            >
+                <StyledRating
+                    defaultValue={2}
+                    getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+                    precision={0.5}
+                    icon={<EnergySavingsLeafIcon fontSize="inherit" />}
+                    emptyIcon={<EnergySavingsLeafIcon fontSize="inherit" />}
+                    readOnly
+                />
+            </Box>
+        </div>
+    );
+}
--- sources/web-front/src/pages/main_page/Presentation.js
+++ sources/web-front/src/pages/main_page/Presentation.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+    import * as React from 'react';
 import presentation from '../../images/presentation.jpg';
 
 export default function Presentation() {
