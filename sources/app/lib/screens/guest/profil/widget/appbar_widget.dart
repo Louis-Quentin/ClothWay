c406398d00e6beaa5f8fbd01ca2f489da5a8346e
redouanee <redouane.el-shahawi@epitech.eu>
2023-05-09 03:14:48 +0200

    [ADD] edditiing profile page

--- sources/app/lib/screens/guest/profil/widget/appbar_widget.dart
+++ sources/app/lib/screens/guest/profil/widget/appbar_widget.dart
@@ -1,7 +1,11 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:bis/screens/guest/profil/themes.dart';
+import 'package:bis/screens/guest/profil/utils/user_preferences.dart';
 
 AppBar buildAppBar(BuildContext context) {
+  final isDarkMode = Theme.of(context).brightness == Brightness.dark;
   final icon = CupertinoIcons.moon_stars;
 
   return AppBar(
@@ -9,9 +13,16 @@ AppBar buildAppBar(BuildContext context) {
     backgroundColor: Colors.transparent,
     elevation: 0,
     actions: [
-      IconButton(
-        icon: Icon(icon),
-        onPressed: () {},
+      ThemeSwitcher(
+        builder: (context) => IconButton(
+          icon: Icon(icon),
+          onPressed: () {
+            final theme = isDarkMode ? MyThemes.lightTheme : MyThemes.darkTheme;
+
+            final switcher = ThemeSwitcher.of(context)!;
+            switcher.changeTheme(theme: theme);
+          },
+        ),
       ),
     ],
   );
--- sources/app/lib/screens/guest/profil/widget/profile_widget.dart
+++ sources/app/lib/screens/guest/profil/widget/profile_widget.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 
 class ProfileWidget extends StatelessWidget {
   final String imagePath;
+  final bool isEdit;
   final VoidCallback onClicked;
 
   const ProfileWidget({
     Key? key,
     required this.imagePath,
+    this.isEdit = false,
     required this.onClicked,
   }) : super(key: key);
 
@@ -54,7 +56,7 @@ class ProfileWidget extends StatelessWidget {
           color: color,
           all: 8,
           child: Icon(
-            Icons.edit,
+            isEdit ? Icons.add_a_photo : Icons.edit,
             color: Colors.white,
             size: 20,
           ),
