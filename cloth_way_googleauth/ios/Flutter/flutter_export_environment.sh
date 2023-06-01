c2f0869a622855a49574572a379dd01e7aafba0c
Rémi Desbordes <malloc@MacBook-Pro-de-Remi.local>
2023-06-01 11:03:24 +0200

    feat added: Button to see contacts in google sign in project

--- cloth_way_googleauth/ios/Flutter/flutter_export_environment.sh
+++ cloth_way_googleauth/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.7.12/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/malloc/Documents/EIP/cloth_way_googleauth"
+export "FLUTTER_APPLICATION_PATH=/Users/malloc/Documents/EIP/ClothWay/cloth_way_googleauth"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/malloc/Documents/EIP/cloth_way_googleauth/lib/main.dart"
+export "FLUTTER_TARGET=/Users/malloc/Documents/EIP/ClothWay/cloth_way_googleauth/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/malloc/Documents/EIP/cloth_way_googleauth/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/malloc/Documents/EIP/ClothWay/cloth_way_googleauth/.dart_tool/package_config.json"
--- cloth_way_googleauth/lib/main.dart
+++ cloth_way_googleauth/lib/main.dart
@@ -1,88 +1,79 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// ignore_for_file: avoid_print
-
+// Importation de bibliothèques nécessaires
+import 'package:flutter/material.dart'; // Importation Flutter
+import 'package:http/http.dart' as http; // Importation pour les requêtes HTTP
+import 'dart:convert' show json; // Importation manipulation de JSON
+import 'package:google_sign_in/google_sign_in.dart'; // Importation Google Signi In
 import 'dart:async';
-import 'dart:convert' show json;
-
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:http/http.dart' as http;
-
 import 'src/sign_in_button.dart';
 
-/// The scopes required by this application.
+// Définition des scopes nécessaires pour l'authentification avec Google
 const List<String> scopes = <String>[
   'email',
   'https://www.googleapis.com/auth/contacts.readonly',
 ];
 
+// Création d'une instance de GoogleSignIn avec les scopes
 GoogleSignIn _googleSignIn = GoogleSignIn(
-  // Optional clientId
-  // clientId: 'your-client_id.apps.googleusercontent.com',
   scopes: scopes,
 );
 
 void main() {
+  // Exécute l'application Flutter avec une interface utilisateur MaterialApp
   runApp(
     const MaterialApp(
       title: 'Google Sign In',
-      home: SignInDemo(),
+      home: test(), // Utilise la classe test comme page d'accueil
     ),
   );
 }
 
-/// The SignInDemo app.
-class SignInDemo extends StatefulWidget {
-  ///
-  const SignInDemo({super.key});
+// Classe pour la page principale
+class test extends StatefulWidget {
+  const test({super.key});
 
   @override
-  State createState() => _SignInDemoState();
+  State createState() => _testState(); 
 }
 
-class _SignInDemoState extends State<SignInDemo> {
-  GoogleSignInAccount? _currentUser;
-  bool _isAuthorized = false; // has granted permissions?
-  String _contactText = '';
+// Classe pour l'état de la page de la connexion Google
+class _testState extends State<test> {
+  GoogleSignInAccount? _currentUser; // Compte Google
+  bool _isAuthorized = false; // Autorisations
+  bool _isPopupVisible = false;
+  String _contactText = ''; // String pour le contact
+  Map<String, dynamic>? _decodedData;
 
   @override
   void initState() {
     super.initState();
 
-    _googleSignIn.onCurrentUserChanged
-        .listen((GoogleSignInAccount? account) async {
-      // In mobile, being authenticated means being authorized...
+    // Si jamais l'utilisateur change
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) async {
       bool isAuthorized = account != null;
-      // However, in the web...
-      if (kIsWeb && account != null) {
-        isAuthorized = await _googleSignIn.canAccessScopes(scopes);
-      }
 
-      setState(() {
-        _currentUser = account;
-        _isAuthorized = isAuthorized;
-      });
+      // Vérifie si l'utilisateur est autorisé sur le Web en vérifiant les scopes
+        if (kIsWeb && account != null) {
+          isAuthorized = await _googleSignIn.canAccessScopes(scopes);
+        }
 
-      // Now that we know that the user can access the required scopes, the app
-      // can call the REST API.
-      if (isAuthorized) {
-        _handleGetContact(account!);
-      }
+        // Met à jour l'état de l'application avec le compte et l'autorisation
+        setState(() {
+          _currentUser = account;
+          _isAuthorized = isAuthorized;
+        });
+
+        if (isAuthorized) {
+          _handleGetContact(account!);
+          // Si l'utilisateur est autorisé, appel de la fonction _handleGetContact avec compte le google.
+        }
     });
 
-    // In the web, _googleSignIn.signInSilently() triggers the One Tap UX.
-    //
-    // It is recommended by Google Identity Services to render both the One Tap UX
-    // and the Google Sign In button together to "reduce friction and improve
-    // sign-in rates" ([docs](https://developers.google.com/identity/gsi/web/guides/display-button#html)).
     _googleSignIn.signInSilently();
+    // Auth invisible
   }
 
-  // Calls the People API REST endpoint for the signed-in user to retrieve information.
+  // Call API pour récup les informations
   Future<void> _handleGetContact(GoogleSignInAccount user) async {
     setState(() {
       _contactText = 'Loading contact info...';
@@ -102,40 +93,13 @@ class _SignInDemoState extends State<SignInDemo> {
     }
     final Map<String, dynamic> data =
         json.decode(response.body) as Map<String, dynamic>;
-    final String? namedContact = _pickFirstNamedContact(data);
+    print('La tah data : $data');
     setState(() {
-      if (namedContact != null) {
-        _contactText = 'I see you know $namedContact!';
-      } else {
-        _contactText = 'No contacts to display.';
-      }
+      _decodedData = data;
     });
   }
 
-  String? _pickFirstNamedContact(Map<String, dynamic> data) {
-    final List<dynamic>? connections = data['connections'] as List<dynamic>?;
-    final Map<String, dynamic>? contact = connections?.firstWhere(
-      (dynamic contact) => (contact as Map<Object?, dynamic>)['names'] != null,
-      orElse: () => null,
-    ) as Map<String, dynamic>?;
-    if (contact != null) {
-      final List<dynamic> names = contact['names'] as List<dynamic>;
-      final Map<String, dynamic>? name = names.firstWhere(
-        (dynamic name) =>
-            (name as Map<Object?, dynamic>)['displayName'] != null,
-        orElse: () => null,
-      ) as Map<String, dynamic>?;
-      if (name != null) {
-        return name['displayName'] as String?;
-      }
-    }
-    return null;
-  }
-
-  // This is the on-click handler for the Sign In button that is rendered by Flutter.
-  //
-  // On the web, the on-click handler of the Sign In button is owned by the JS
-  // SDK, so this method can be considered mobile only.
+  // Gère l'événement de clic sur le bouton de connexion
   Future<void> _handleSignIn() async {
     try {
       await _googleSignIn.signIn();
@@ -144,12 +108,7 @@ class _SignInDemoState extends State<SignInDemo> {
     }
   }
 
-  // Prompts the user to authorize `scopes`.
-  //
-  // This action is **required** in platforms that don't perform Authentication
-  // and Authorization at the same time (like the web).
-  //
-  // On the web, this must be called from an user interaction (button click).
+  // Demande à l'utilisateur d'autoriser les scopes requis
   Future<void> _handleAuthorizeScopes() async {
     final bool isAuthorized = await _googleSignIn.requestScopes(scopes);
     setState(() {
@@ -160,12 +119,14 @@ class _SignInDemoState extends State<SignInDemo> {
     }
   }
 
+  // Déconnecte l'utilisateur
   Future<void> _handleSignOut() => _googleSignIn.disconnect();
 
+  // Construit le contenu de la page en fonction de l'état de connexion de l'utilisateur
   Widget _buildBody() {
     final GoogleSignInAccount? user = _currentUser;
     if (user != null) {
-      // The user is Authenticated
+      // L'utilisateur est connecté
       return Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
@@ -178,16 +139,21 @@ class _SignInDemoState extends State<SignInDemo> {
           ),
           const Text('Signed in successfully.'),
           if (_isAuthorized) ...<Widget>[
-            // The user has Authorized all required scopes
+            // L'utilisateur a autorisé tous les scopes requis
             Text(_contactText),
             ElevatedButton(
               child: const Text('REFRESH'),
-              onPressed: () => _handleGetContact(user),
+              onPressed: () {
+                setState(() {
+                  _isPopupVisible = true;
+                });
+                _handleGetContact(user);
+              },
             ),
           ],
           if (!_isAuthorized) ...<Widget>[
-            // The user has NOT Authorized all required scopes.
-            // (Mobile users may never see this button!)
+            // L'utilisateur n'a pas autorisé tous les scopes requis.
+            // (Les utilisateurs mobiles ne verront peut-être jamais ce bouton !)
             const Text('Additional permissions needed to read your contacts.'),
             ElevatedButton(
               onPressed: _handleAuthorizeScopes,
@@ -201,13 +167,13 @@ class _SignInDemoState extends State<SignInDemo> {
         ],
       );
     } else {
-      // The user is NOT Authenticated
+      // L'utilisateur n'est pas connecté
       return Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           const Text('You are not currently signed in.'),
-          // This method is used to separate mobile from web code with conditional exports.
-          // See: src/sign_in_button.dart
+          // Méthode utilisée pour différencier le code mobile du code web avec des exports conditionnels.
+          // Voir : src/sign_in_button.dart
           buildSignInButton(
             onPressed: _handleSignIn,
           ),
@@ -216,15 +182,53 @@ class _SignInDemoState extends State<SignInDemo> {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Google Sign In'),
-        ),
-        body: ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: _buildBody(),
-        ));
+    @override
+  //   Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text('Google Sign In'),
+  //     ),
+  //     body: ConstrainedBox(
+  //       constraints: const BoxConstraints.expand(),
+  //       child: _buildBody(),
+  //     ),
+  //   );
+  // }
+    Widget build(BuildContext context) {
+  // ...
+  Widget dataTextWidget;
+  if (_decodedData != null) {
+    dataTextWidget = Text('Decoded data: $_decodedData');
+  } else {
+    dataTextWidget = Text('No data available');
   }
+
+  return Scaffold(
+    // ...
+    // ...
+    body: Builder(
+      builder: (BuildContext context) {
+        if (_isPopupVisible) {
+          return AlertDialog(
+            title: Text('Decoded Data'),
+            content: dataTextWidget,
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _isPopupVisible = false;
+                  });
+                },
+                child: Text('Close'),
+              ),
+            ],
+          );
+        } else {
+          return _buildBody(); // Return an empty widget if _isPopupVisible is false
+        }
+      },
+    )
+    
+  );
+}
 }
